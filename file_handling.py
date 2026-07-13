7d45562fe064e48b2ae4aa1c06403a113278dccc
Adityabisht794 <adityabisht220111794@gmail.com>
2026-07-13 21:58:00 +0530

    File Handling

---
+++ file_handling.py
@@ -0,0 +1,41 @@
+file_name=input("Enter the file name: ")
+try:
+    f_handle=open(file_name,"r+")
+except:
+    print("File cannot be opened:", file_name)
+    quit()
+
+count=0
+
+for line in f_handle:
+    count=count+1
+    print(line)
+
+print("\nThere were", count, "lines in", file_name)
+
+print("\n")
+f_handle.seek(0)  # Reset file pointer to the beginning of the file
+count=0
+
+for line in f_handle:
+    if line.startswith("-"):
+        count=count+1
+
+print("There were", count, "- lines in", file_name)
+
+f_handle.seek(0)  
+for line in f_handle:
+    if line.startswith("-"):
+        line="*" + line[1:] 
+        print(line) 
+    
+
+lines=["\nThis is a new line 1\n", "This is a new line 2\n", "This is a new line 3\n"]
+f_handle.writelines(lines)
+
+f_handle.seek(0)  # Reset file pointer to the beginning of the file
+print("\nUpdated file content:")
+for line in f_handle:
+    print(line)
+
+f_handle.close()
